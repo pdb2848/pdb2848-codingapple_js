dd33cae51011aacb170a7716dd933d79c30d6aa9
pdb2848 <dongbin4573@gmail.com>
2026-01-09 06:26:44 +0900

    Learn Array Object
    데이터 바인딩 강의 까지

--- tab.js
+++ tab.js
@@ -1,29 +1,27 @@
 var tabBtn = $('.tab-button')
 var tabCon = $('.tab-content')
 
-for (let i = 0; i < tabBtn.length; i++){
-    $('.tab-button').eq(i).on('click', function(){
-        tabBtn.removeClass('orange')
-        tabBtn.eq(i).addClass('orange')
-        tabCon.removeClass('show')
-        tabCon.eq(i).addClass('show')
-    })
+var car = ['소나타', 50000, 'white']
+var car2 = {name : '소나타', price : 50000}
+
+$('.car-title').html(car2.name + ' / ' + car2.price)
+
+// for (let i = 0; i < tabBtn.length; i++){
+//     $('.tab-button').eq(i).on('click', function(){
+//         openTab(i)
+//     })
+// }
+
+
+$('.list').click(function(e){
+    openTab(e.target.dataset.id)
+})
+
+
+function openTab(num){
+    tabBtn.removeClass('orange')
+    tabBtn.eq(num).addClass('orange')
+    tabCon.removeClass('show')
+    tabCon.eq(num).addClass('show')
 }
-// $('.tab-button').eq(0).on('click', function(){
-//     tabBtn.removeClass('orange')
-//     tabBtn.eq(0).addClass('orange')
-//     tabCon.removeClass('show')
-//     tabCon.eq(0).addClass('show')
-// })
-// $('.tab-button').eq(1).on('click', function(){
-//     tabBtn.removeClass('orange')
-//     tabBtn.eq(1).addClass('orange')
-//     tabCon.removeClass('show')
-//     tabCon.eq(1).addClass('show')
-// })
-// $('.tab-button').eq(2).on('click', function(){
-//     tabBtn.removeClass('orange')
-//     tabBtn.eq(2).addClass('orange')
-//     tabCon.removeClass('show')
-//     tabCon.eq(2).addClass('show')
-// })
+
