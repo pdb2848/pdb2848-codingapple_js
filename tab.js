a7a1641b6db40630ca20aaf779313a53b752152f
pdb2848 <dongbin4573@gmail.com>
2026-01-10 21:27:32 +0900

    Learn select for each
    select 태그와 for each for in 까지

--- tab.js
+++ tab.js
@@ -17,6 +17,39 @@ $('.list').click(function(e){
     openTab(e.target.dataset.id)
 })
 
+//select
+var product_select = $('.form-select').eq(0)
+var size_select = $('.form-select').eq(1) 
+var pants = [28, 30, 32, 34]
+var shirts = [95, 100, 105]
+
+product_select.on('input', function(){
+    if (product_select.val() == '셔츠'){
+        size_select.removeClass('form-hide')
+        size_select.html('')
+        shirts.forEach(function(size){
+            size_select.append(`<option>${size}</option>`)
+        })
+    }
+    else if(product_select.val() == '바지'){
+        size_select.removeClass('form-hide')
+        size_select.html('')
+        pants.forEach(function(size){
+            size_select.append(`<option>${size}</option>`)
+        })
+    }
+    else{
+        size_select.addClass('form-hide')
+    }
+})
+
+var testP = '<option>28</option>'
+testP.innerHTML = 'hi'
+document.querySelector('#test').appendChild(testP)
+
+
+
+
 
 function openTab(num){
     tabBtn.removeClass('orange')
